f3ea74712a947404c8c88d55cf07577a30351770
Rush K <k_rush@naver.com>
2021-08-15 00:37:29 +0900

    메인 페이지 구현

---
+++ src/components/Main.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { Button, InputLabel, MenuItem, FormControl, Select,
+  Container, Paper, Divider, Typography } from '@material-ui/core';
+import { Send } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+import '@fontsource/roboto';
+
+const useStyles = theme => ({
+    root: {
+      display: 'flex',
+      height: '100vh',
+      alignItems: "center",
+      justifyContent: "center",
+      '& > *': {
+        display: "flex",
+        justifyContent: "flex-start",
+        flexDirection:"column",
+        margin: theme.spacing(1),
+        width: theme.spacing(80),
+        height: theme.spacing(40),
+        '& > div': {
+          display:"flex",
+          justifyContent: "center",
+          width: "100%",
+          margin: theme.spacing(1),
+        }
+      },
+    },
+    formControl: {
+      margin: theme.spacing(2),
+      width: "100%",
+    },
+    button: {
+      margin: theme.spacing(1),
+    },
+  });
+
+class Main extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        stockList: [],
+        selectedStock: null
+      }
+
+    }
+
+    selectStock = (event) => {
+      this.setState({selectedStock: event.target.value});
+    }
+
+    render () {
+        const { classes } = this.props;
+
+        return (
+            <Container fixed className={classes.root}>
+                <Paper elevation={3}>
+                  <div>
+                    <Typography variant="h3">STOCK ANALYZER</Typography>
+                  </div>
+                  <Divider />
+
+                  <div>
+                    <Typography variant="h6">다양한 주식 분석 정보를 알려드려요!</Typography>
+                  </div>
+                  <Divider />
+
+                  {/* 주식 선택 드롭박스 */}
+                  <div>
+                    <FormControl variant="outlined" className={classes.formControl}>
+                      <InputLabel id="stock-list-label">열람할 주식을 선택하세요.</InputLabel>
+                        <Select
+                          labelId="stock-list-label"
+                          id="stock-list"
+                          value={this.state.selectedStock}
+                          onChange={this.selectStock}
+                          label="stock-list-label"
+                        >
+                        <MenuItem value="">
+                          <em>주식 선택</em>
+                        </MenuItem>
+                        <MenuItem value={'삼성전자'}>삼성전자</MenuItem>
+                        <MenuItem value={'SK하이닉스'}>SK하이닉스</MenuItem>
+                        <MenuItem value={'두산중공업'}>두산중공업</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </div>
+
+
+                  <div>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className={classes.button}
+                      endIcon={<Send/>}>
+                      열람하기
+                    </Button>
+                  </div>
+
+                </Paper>
+            </Container>
+        );
+    }
+}
+
+export default withStyles(useStyles)(Main);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({ // 공용 테마 생성 
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        body: {
+          width: '100vw',
+          height: '100vh',
+          backgroundColor: 'gray',
+        }
+      }
+    }
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +32,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
