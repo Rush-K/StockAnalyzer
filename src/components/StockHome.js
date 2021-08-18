5d6d1816538a1bfc674b96fa1e52bb8c1fc38add
Rush K <k_rush@naver.com>
2021-08-18 20:48:40 +0900

    코스피, 코스닥 지수 레이아웃 추가

--- src/components/StockHome.js
+++ src/components/StockHome.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import { Container, Divider, Paper, Typography,
+import { Container, Divider, Paper, Typography, Button,
         List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { ChangeHistory, TrendingUp } from '@material-ui/icons';
+import { ChangeHistory, TrendingUp, LibraryBooks } from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 
 import DailyChart from './DailyChart';
+import TriangularConvergenceChart from './TriangularConvergenceChart';
 
 const useStyles = theme => ({
     root: {
@@ -14,39 +16,53 @@ const useStyles = theme => ({
       justifyContent: "center",
       '& > *': {
         display: "flex",
+        flexWrap: "wrap",
         justifyContent: "flex-start",
         margin: theme.spacing(1),
         width: theme.spacing(100),
-        height: theme.spacing(60),
+        height: theme.spacing(80),
       },
+    },
+    indexPaper: {
+        display:"flex",
+        margin: theme.spacing(1),
+        width: theme.spacing(48),
+        height: "20%"
     },
     menuPaper: {
         display:"flex",
         flexDirection: "column",
         alignItems: "flex-start",
-        width: theme.spacing(30),
-        height: "100%",
+        width: theme.spacing(29),
+        height: "80%",
         margin: theme.spacing(1),
-        '& > *': {
+        '& > div#list': {
+            width: "100%",
+        },
+        '& > div#stockname': {
             display: "flex",
             justifyContent: "center",
             width: "100%",
+        },
+        '& > div#back': {
+            display: "flex",
+            flexWrap: "wrap",
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "flex-end",
         }
       },
     homePaper: {
         display:"flex",
         alignItems: "center",
         justifyContent: "center",
-        width: theme.spacing(70),
-        height: "100%",
+        width: theme.spacing(67),
+        height: "80%",
         margin: theme.spacing(1),
         '& > *': {
             width: "100%",
         }
-    },
-    formControl: {
-      margin: theme.spacing(2),
-      width: "100%",
     },
     button: {
       margin: theme.spacing(1),
@@ -58,11 +74,18 @@ class StockHome extends Component {
         super(props);
         this.state = {
             DailyChart : false,
+            TriangularConvergenceChart : false,
         }
     }
 
     changeDailyChartState = (event) => {
-        this.setState({DailyChart: !this.state.DailyChart});
+        this.setState({DailyChart: !this.state.DailyChart,
+                       TriangularConvergenceChart: false});
+    }
+
+    changeTriangularConvergenceChartState = (event) => {
+        this.setState({DailyChart: false,
+                       TriangularConvergenceChart: !this.state.TriangularConvergenceChart});
     }
 
     render() {
@@ -70,13 +93,20 @@ class StockHome extends Component {
         return (
             <Container className={classes.root}>
                 <div>
+                    {/* KOSPI COSDAQ 지수 */}
+                    <Paper className={classes.indexPaper}>
+                        <Typography>KOSPI 지수</Typography>
+                    </Paper>
+                    <Paper className={classes.indexPaper}>
+                        <Typography>KOSDAQ 지수</Typography>
+                    </Paper>
                     {/* 메뉴 */}
                     <Paper elevation={3} className={classes.menuPaper}>
-                        <div>
+                        <div id="stockname">
                             <Typography variant="h6">{this.props.match.params.stockcode}</Typography>
                         </div>
-                        <Divider />
-                        <div>
+                        <Divider style={{width:"100%"}}/>
+                        <div id="list">
                             <List>
                                 <ListItem button onClick={this.changeDailyChartState}>
                                     <ListItemIcon>
@@ -84,20 +114,39 @@ class StockHome extends Component {
                                     </ListItemIcon>
                                     <ListItemText primary="일자별 주가그래프" />
                                 </ListItem>
-                                <ListItem button>
+                                <ListItem button onClick={this.changeTriangularConvergenceChartState}>
                                     <ListItemIcon>
                                         <ChangeHistory />
                                     </ListItemIcon>
                                     <ListItemText primary="삼각수렴 확인" />
                                 </ListItem>
+                                <ListItem button>
+                                    <ListItemIcon>
+                                        <LibraryBooks />
+                                    </ListItemIcon>
+                                    <ListItemText primary="신용 매매동향 확인" />
+                                </ListItem>
+                                <ListItem button divider>
+                                    <ListItemIcon>
+
+                                    </ListItemIcon>
+                                    <ListItemText primary="투자자 매매현황 확인" />
+                                </ListItem>
                             </List>
                         </div>
-
+                        <div id="back">
+                            <Link to="/">
+                                <Button>
+                                    이전 페이지로
+                                </Button>
+                            </Link>
+                        </div>
                     </Paper>
 
                     {/* 홈 */}
                     <Paper elevation={3} className={classes.homePaper}>
                         {this.state.DailyChart === true && <DailyChart changeDailyChartState={this.changeDailyChartState} stockcode={this.props.match.params.stockcode}/>}
+                        {this.state.TriangularConvergenceChart === true && <TriangularConvergenceChart changeTriangularConvergenceChartState={this.changeTriangularConvergenceChartState} stockcode={this.props.match.params.stockcode} />}
                     </Paper>
                 </div>
             </Container>
