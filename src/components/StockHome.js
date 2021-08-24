e154ace8c571b6c1fe1f2627ef193cf815da48a5
kms <k_rush@naver.com>
2021-08-24 16:21:19 +0900

    KOSPI, KOSDAQ API 적용

--- src/components/StockHome.js
+++ src/components/StockHome.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Container, Divider, Paper, Typography, Button,
-        List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { ChangeHistory, TrendingUp, LibraryBooks, HowToReg } from '@material-ui/icons';
+        List, ListItem, ListItemIcon, ListItemText, CircularProgress } from '@material-ui/core';
+import { ArrowDropUp, ArrowDropDown,ChangeHistory, TrendingUp, LibraryBooks, HowToReg } from '@material-ui/icons';
 import { withStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 
 import DailyChart from './DailyChart';
 import TriangularConvergenceChart from './TriangularConvergenceChart';
 import CreditTradingChart from './CreditTradingChart';
+import axios from 'axios';
 
 const useStyles = theme => ({
     root: {
@@ -36,7 +37,14 @@ const useStyles = theme => ({
             justifyContent: "center",
             '& > div#title_text': {
                 display: "flex",
+                width: "100%",
+                flexDirection: "column",
+                alignItems: "center",
                 justifyContent: "center",
+                '& > div#index_text': {
+                    display: "flex",
+                    justifyContent: "center",
+                }
             }
         }
     },
@@ -84,6 +92,8 @@ class StockHome extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            StockName: null,
+            Index: null,
             DailyChart : false,
             TriangularConvergenceChart : false,
             CreditTradingChart : false, 
@@ -113,10 +123,34 @@ class StockHome extends Component {
             CreditTradingChart: !this.state.CreditTradingChart
         });
     }
+    
+    componentDidMount = async() => {
+        let stockname = await axios.get('http://localhost:5000/stockname/' + this.props.match.params.stockcode)
+                                   .then(function (response) {
+                                       return response.data;
+                                   })
+                                   .catch(function (error) {
+                                       console.log(error);
+                                   })
+
+        let index = await axios.get('http://localhost:5000/index')
+                               .then(function (response) {
+                                    return response.data;
+                               })
+                               .catch(function (error) {
+                                    console.log(error);
+                               })
+
+        this.setState({StockName: stockname, Index: index});
+    }
 
     render() {
         const { classes } = this.props;
         return (
+            this.state.stockname === null ?
+            <div className={classes.root}>
+                <CircularProgress />
+            </div> :
             <Container className={classes.root}>
                 <div>
                     {/* KOSPI COSDAQ 지수 */}
@@ -126,9 +160,21 @@ class StockHome extends Component {
                                 <Typography variant="h7">KOSPI</Typography>
                             </div>
                             <Divider />
+                            {this.state.Index === null ?
                             <div id="title_text">
-                                <Typography variant="h1">3000.0</Typography>
-                            </div>
+                                <CircularProgress /> 
+                            </div> :
+                            <div id="title_text">
+                                <div id="index_text">
+                                    <Typography variant="h2">{this.state.Index[0].KOSPIprice}</Typography>
+                                </div>
+                                <div id ="index_text">
+                                    {this.state.Index[0].KOSPIctpd.includes("+") ? 
+                                    <ArrowDropUp color="secondary" /> :
+                                    <ArrowDropDown color="primary" />}
+                                    <Typography variant="h6">{this.state.Index[0].KOSPIctpd.replace("+", "").replace("-", "")}</Typography>
+                                </div>
+                            </div>}
                         </div>
                     </Paper>
                     <Paper className={classes.indexPaper}>
@@ -137,16 +183,28 @@ class StockHome extends Component {
                                 <Typography variant="h7">KOSDAQ</Typography>
                             </div>
                             <Divider />
+                            {this.state.Index === null ?
                             <div id="title_text">
-                                <Typography variant="h1">1000.0</Typography>
-                            </div>
+                                <CircularProgress /> 
+                            </div> :
+                            <div id="title_text">
+                                <div id="index_text">
+                                    <Typography variant="h2">{this.state.Index[0].KOSDAQprice}</Typography>
+                                </div>
+                                <div id ="index_text">
+                                    {this.state.Index[0].KOSDAQctpd.includes("+") ? 
+                                    <ArrowDropUp color="secondary" /> :
+                                    <ArrowDropDown color="primary" />}
+                                    <Typography variant="h6">{this.state.Index[0].KOSDAQctpd.replace("+", "").replace("-", "")}</Typography>
+                                </div>
+                            </div>}
                         </div>
                     </Paper>
 
                     {/* 메뉴 */}
                     <Paper elevation={3} className={classes.menuPaper}>
                         <div id="stockname">
-                            <Typography variant="h6">{this.props.match.params.stockcode}</Typography>
+                            <Typography variant="h6">{this.state.StockName}</Typography>
                         </div>
                         <Divider style={{width:"100%"}}/>
                         <div id="list">
