67bceaf455699b20e578d4a611322fbd996b4740
kms <k_rush@naver.com>
2021-08-27 16:43:33 +0900

    일봉 데이터 조회 수정 - 거래일 범위 추가

--- src/components/DailyChart.js
+++ src/components/DailyChart.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import Chart from 'react-apexcharts';
 import axios from 'axios';
+import { withStyles } from '@material-ui/styles';
 
+const useStyles = theme => ({
+  root: {
+    display:"flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    margin: theme.spacing(1),
+    '& > div#day_input': {
+      margin: theme.spacing(3),
+    }
+  }
+});
 
 class DailyChart extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
+          day: null,
             series: [{
                 data: []
             }],
@@ -37,25 +53,36 @@ class DailyChart extends Component {
         this.props.changeDailyChartState();
     }
 
-    componentDidMount = async() => {
-      let data = await axios.get('http://localhost:5000/stockdailycandle/' + this.props.stockcode)
-                            .then(function (response) {
-                              return response.data;
-                            })
-                            .catch(function (error) {
-                              console.log(error);
-                            })
-      let stringData = JSON.stringify(data).replaceAll("일자", "x").replaceAll("시가\":", "y\":[")
-                       .replaceAll("\"현재가\":", "").replaceAll("\"고가\":", "").replaceAll("\"저가\":", "").replaceAll("}", "]}");
-      let jsonData = JSON.parse(stringData);
-      this.setState({series:[{data: jsonData}]});
+    changeDayState = (event) => {
+      this.setState({day: event.target.value});
+    }
 
+    loadChart = async() => {
+      if (this.state.day == null || isNaN(this.state.day) || this.state.day < 1 || this.state.day > 600) {
+        alert("1 ~ 600 사이의 숫자만 입력해주세요.");
+      } else {
+        let data = await axios.get('http://localhost:5000/stockdailycandle/' + this.props.stockcode + '/' + this.state.day)
+                                  .then(function (response) {
+                                      return response.data;
+                                  })
+                                  .catch(function (error) {
+                                      console.log(error);
+                                  });
+        let stringData = JSON.stringify(data).replaceAll("일자", "x").replaceAll("시가\":", "y\":[")
+        .replaceAll("\"현재가\":", "").replaceAll("\"고가\":", "").replaceAll("\"저가\":", "").replaceAll("}", "]}");
+        let jsonData = JSON.parse(stringData);
+        this.setState({series:[{data: jsonData}]});
+      }
     }
 
     render() {
-        console.log(this.state);
+        const { classes } = this.props;
         return (
-            <div id="chart">
+            <div className={classes.root} id="chart">
+                <div id="day_input">
+                  <TextField onChange={this.changeDayState} size="small" id="outlined-basic" label="거래일 입력" variant="outlined" />
+                  <Button onClick={this.loadChart} style={{marginLeft: "3px"}} variant="outlined" color="primary" size="large">입력</Button>
+                </div>
                 <Chart options={this.state.options} series={this.state.series} type="candlestick" width="96%" height={350} />
                 <Button onClick={this.sendDailyChartState} variant="outlined" color="secondary" style={{margin: "10px", width: "96%"}}>차트 닫기</Button>
             </div>
@@ -63,4 +90,4 @@ class DailyChart extends Component {
     }
 }
 
-export default DailyChart
+export default withStyles(useStyles)(DailyChart)
