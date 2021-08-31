45a473fdf527fe3f3e24adffa826e5063ea5c6b2
kms <k_rush@naver.com>
2021-08-31 18:19:28 +0900

    신용 매매동향 API 적용

--- src/components/CreditTradingChart.js
+++ src/components/CreditTradingChart.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { Button, Typography } from '@material-ui/core';
+import { Button, TextField, Typography } from '@material-ui/core';
 import Chart from 'react-apexcharts';
 import { withStyles } from '@material-ui/styles';
+import axios from 'axios';
+
 
 const useStyles = theme => ({
     root: {
@@ -15,6 +17,9 @@ const useStyles = theme => ({
             display: "flex",
             width: "100%",
             justifyContent: "center",
+        },
+        '& > div#day_input': {
+            margin: theme.spacing(3),
         }
     }
 });
@@ -23,30 +28,29 @@ class CreditTradingChart extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            day: null,
             lenderOrLoan: false,
             lenderOptions: {
                 chart: {
                   id: "basic-bar"
                 },
                 xaxis: {
-                  categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-                }
+                    type: 'datetime'
+                },
             },
             lenderSeries: [{
-                name: "대주잔고",
-                data: [30, 40, 45, 50, 49, 60, 70, 91]
+                data: []
             }],
             loanOptions: {
                 chart: {
                   id: "basic-bar"
                 },
                 xaxis: {
-                  categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-                }
+                    type: 'datetime'
+                },
             },
             loanSeries: [{
-                name: "융자잔고",
-                data: [30, 40, 45, 50, 49, 60, 70, 100]
+                data: []
             }],
         }
     }
@@ -59,19 +63,69 @@ class CreditTradingChart extends Component {
         this.setState({lenderOrLoan: !this.state.lenderOrLoan});
     }
 
+    changeDayState = (event) => {
+        this.setState({day: event.target.value});
+    }
+
+    loadChart = async() => {
+        if (this.state.day == null || isNaN(this.state.day) || this.state.day < 1 || this.state.day > 600) {
+            alert("1 ~ 600 사이의 숫자만 입력해주세요.");
+        } else {
+            let type_zero_data = await axios.get('http://localhost:5000/credittrading/0/' + this.props.stockcode + '/' + this.state.day)
+                                    .then(function (response) {
+                                        return response.data;
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    }); // 융자잔고 데이터
+
+            let type_one_data = await axios.get('http://localhost:5000/credittrading/1/' + this.props.stockcode + '/' + this.state.day)
+                                    .then(function (response) {
+                                        return response.data;
+                                    })
+                                    .catch(function (error) {
+                                        console.log(error);
+                                    }); // 대주잔고 데이터
+
+            type_zero_data = JSON.stringify(type_zero_data).replaceAll("일자", "x").replaceAll("융자잔고", "y");
+            type_one_data = JSON.stringify(type_one_data).replaceAll("일자", "x").replaceAll("대주잔고", "y");
+            
+
+            let json_type_zero_data = JSON.parse(type_zero_data);
+            let json_type_one_data = JSON.parse(type_one_data);
+
+
+            this.setState({
+                loanSeries: [{
+                    data: json_type_zero_data
+                }]
+            });
+
+            this.setState({
+                lenderSeries: [{ 
+                    data: json_type_one_data
+                }],
+            });
+        }
+      }
+
     render() {
         const { classes } = this.props;
 
         return (
             <div id="chart" className={classes.root}>
+                <div id="day_input">
+                    <TextField onChange={this.changeDayState} size="small" id="outlined-basic" label="거래일 입력" variant="outlined" />
+                    <Button onClick={this.loadChart} style={{marginLeft: "3px"}} variant="outlined" color="primary" size="large">입력</Button>
+                </div>
                 <div id="changeButton">
                     {this.state.lenderOrLoan === false ?
                     <Button variant="contained" color="primary" onClick={this.changeLenderOrLoanChartState} style={{margin: "10px"}}>대주 잔고</Button> :
                     <Button variant="contained" color="primary" onClick={this.changeLenderOrLoanChartState} style={{margin: "10px"}}>융자 잔고</Button>}
                 </div>
                 {this.state.lenderOrLoan === false ?
-                <Chart options={this.state.lenderOptions} series={this.state.lenderSeries} width={500} height={350} /> :
-                <Chart options={this.state.loanOptions} series={this.state.loanSeries} width={500} height={350} />}
+                <Chart options={this.state.lenderOptions} series={this.state.lenderSeries} width={500} height={290} /> :
+                <Chart options={this.state.loanOptions} series={this.state.loanSeries} width={500} height={290} />}
                 <Button onClick={this.sendCreditTradingChartState} variant="outlined" color="secondary" style={{margin: "10px", width: "96%"}}>차트 닫기</Button>
             </div>
         );
