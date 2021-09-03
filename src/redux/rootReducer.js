16112926a3daf4678bf1baf12194fe328f3b8dc8
kms <k_rush@naver.com>
2021-09-03 21:54:59 +0900

    Redux Store Reset 구현

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,23 @@
 import { combineReducers } from "redux";
 import dailyChartReducer from './DailyChart/reducer';
 
+export const CLEAR_DATA = 'CLEAR_DATA';
 
-const rootReducer = combineReducers({
+export const clearData = () => {
+    return {
+        type: CLEAR_DATA
+    }
+}
+
+const appReducer = combineReducers({
     dailyChart: dailyChartReducer
 });
+
+const rootReducer = (state, action) => {
+    if (action.type === CLEAR_DATA) {
+        state = undefined;
+    }
+    return appReducer(state, action);
+}
 
 export default rootReducer;
